28f2cabc5539042cc7e4649f3a424701001510ef
Kevin Jones <kevin.chris.jones@gmail.com>
2012-09-06 21:38:00 -0400

    Added WCTest.class to ignore

--- WordCounter/src/wordCounterTest/WCTest.java
+++ WordCounter/src/wordCounterTest/WCTest.java
@@ -7,5 +7,10 @@ public class WCTest extends TestCase {
 	public void testMain() {
 		fail("Not yet implemented"); // TODO
 	}
+	
+	public void testCountWords() {
+		fail("Not yet implemented"); // TODO
+		
+	}
 
 }
